91a6e3897cb573ba9d152dd6179b60c96ecb74d3
mglukhikh <glukhikh@mail.ru>
2017-02-13 12:49:38 +0300

    Rational: no throws annotation in tests

--- test/part1/RationalTest.java
+++ test/part1/RationalTest.java
@@ -6,46 +6,46 @@ import static org.junit.Assert.*;
 
 public class RationalTest {
     @Test
-    public void plus() throws Exception {
+    public void plus() {
         assertEquals(new Rational(1, 2),
                 new Rational(1, 6).plus(new Rational(1, 3)));
     }
 
     @Test
-    public void unaryMinus() throws Exception {
+    public void unaryMinus() {
         assertEquals(new Rational(1, 2), new Rational(3, -6).unaryMinus());
     }
 
     @Test
-    public void minus() throws Exception {
+    public void minus() {
         assertEquals(new Rational(1, 3),
                 new Rational(1, 2).minus(new Rational(1, 6)));
     }
 
     @Test
-    public void times() throws Exception {
+    public void times() {
         assertEquals(new Rational(3, 10),
                 new Rational(3, 4).times(new Rational(2, 5)));
     }
 
     @Test
-    public void div() throws Exception {
+    public void div() {
         assertEquals(new Rational(2, 5),
                 new Rational(3, 10).div(new Rational(3, 4)));
     }
 
     @Test
-    public void toInt() throws Exception {
+    public void toInt() {
         assertEquals(1, new Rational(3, 2).toInt());
     }
 
     @Test
-    public void toDouble() throws Exception {
+    public void toDouble() {
         assertEquals(0.5, new Rational(3, 6).toDouble(), 1e-10);
     }
 
     @Test
-    public void equals() throws Exception {
+    public void equals() {
         assertEquals(new Rational(1, 2), new Rational(2, 4));
         assertEquals(new Rational(-1, 2), new Rational(2, -4));
         assertEquals(new Rational(2, 3), new Rational(-4, -6));
