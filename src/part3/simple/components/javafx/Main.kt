02f23bb14007d7e3806ed2f8a68e14891c5f5234
mglukhikh <glukhikh@mail.ru>
2017-04-03 12:45:19 +0300

    JavaFX components example

---
+++ src/part3/simple/components/javafx/Main.kt
@@ -0,0 +1,36 @@
+package part3.simple.components.javafx
+
+import javafx.application.Application
+import javafx.scene.control.Alert
+import javafx.scene.layout.BorderPane
+import tornadofx.*
+
+class ComponentsView : View("Простое окно JavaFX") {
+    override val root = BorderPane()
+
+    init {
+        with (root) {
+            minWidth = 300.0
+            minHeight = 200.0
+            center {
+                vbox {
+                    label("Label")
+                    spacer()
+                    button("Button").setOnAction {
+                        alert(Alert.AlertType.INFORMATION, "", "Button pressed!")
+                    }
+                    spacer()
+                    checkbox("Checkbox")
+                    spacer()
+                    combobox(null, listOf("Combo 1", "Combo 2"))
+                }
+            }
+        }
+    }
+}
+
+class ComponentsApp : App(ComponentsView::class)
+
+fun main(args: Array<String>) {
+    Application.launch(ComponentsApp::class.java, *args)
+}
