8915d89955df1904381831433070b8fea629e38b
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2018-02-12 10:09:30 +0300

    4-in-row computer player: bestTurnMinMax

--- src/part1/fourinrow/core/ComputerPlayer.kt
+++ src/part1/fourinrow/core/ComputerPlayer.kt
@@ -22,8 +22,8 @@ class ComputerPlayer(private val board: Board) {
         if (side == Chip.RED) return -evaluation(Chip.YELLOW)
         var result = random.nextInt(5) - 2
         with (board) {
-            for (x in 0..width - 1) {
-                for (y in 0..height - 1) {
+            for (x in 0 until width) {
+                for (y in 0 until height) {
                     val start = Cell(x, y)
                     for (dir in directions) {
                         val finish = start + dir * (Board.TO_WIN_LENGTH - 1)
@@ -55,6 +55,26 @@ class ComputerPlayer(private val board: Board) {
 
     data class EvaluatedTurn(val turn: Int?, val evaluation: Int)
 
+    fun bestTurnMinMax(depth: Int): EvaluatedTurn {
+        when (board.winner()) {
+            board.turn -> return EvaluatedTurn(null, 10000 + depth)
+            board.turn.opposite() -> return EvaluatedTurn(null, -10000 - depth)
+            else -> {}
+        }
+        if (!board.hasFreeCells()) return EvaluatedTurn(null, 0)
+        if (depth <= 0) return EvaluatedTurn(null, evaluation(board.turn))
+        var result = EvaluatedTurn(null, -1000000)
+        for (turn in 0 until board.width) {
+            if (board.makeTurn(turn) == null) continue
+            val evaluation = -bestTurnMinMax( depth - 1).evaluation
+            board.takeTurnBack(turn)
+            if (evaluation > result.evaluation) {
+                result = EvaluatedTurn(turn, evaluation)
+            }
+        }
+        return result
+    }
+
     @JvmOverloads
     fun bestTurn(depth: Int, lowerBound: Int = -1000000, upperBound: Int = 1000000): EvaluatedTurn {
         when (board.winner()) {
@@ -66,7 +86,7 @@ class ComputerPlayer(private val board: Board) {
         if (depth <= 0) return EvaluatedTurn(null, evaluation(board.turn))
         var lower = lowerBound
         var result = EvaluatedTurn(null, lower)
-        for (turn in 0..board.width - 1) {
+        for (turn in 0 until board.width) {
             if (board.makeTurn(turn) == null) continue
             val evaluation = -bestTurn(
                     depth = depth - 1,
